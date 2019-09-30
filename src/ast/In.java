abb8c87387f1c8d31145faa3607563c30a2e5f0d
Leonardo Zaccarias <leo_zacc8@hotmail.com>
2019-09-30 03:23:56 -0300

    adiciona classes in e out

---
+++ src/ast/In.java
@@ -0,0 +1,20 @@
+/*
+	Nome: Vitor Pratali Camillo	 RA: 620181
+	Nome: Leonardo Zaccarias	 RA: 620491
+*/
+package ast;
+
+public class In {
+
+	public void readInt(){
+		
+	}
+	
+	public void readString(){
+		
+	}
+	public void genJava(PW pw){
+
+	}
+
+}
---
+++ src/ast/Out.java
@@ -0,0 +1,20 @@
+/*
+	Nome: Vitor Pratali Camillo	 RA: 620181
+	Nome: Leonardo Zaccarias	 RA: 620491
+*/
+package ast;
+
+public class Out {
+
+	public void print(){
+		
+	}
+	
+	public void println(){
+		
+	}
+	public void genJava(PW pw){
+
+	}
+
+}
